05f49cde12050d42a69bae3f9ebf6c540a4530c8
hasib824 <hasib824@gmail.com>
2017-06-14 01:57:46 -0700

    kmp
    kmp

---
+++ LightOj/1255 - Substring Frequency.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char text[1000005];
+char pattern[1000005];
+int A[1000005];
+
+int len_text=0; int len_pattern=0;
+
+int kmp()
+{
+    int cnt=0;
+    int i=0; int j=0;
+    while(j<len_text)
+    {
+       if(pattern[j]==text[i]) {i++;j++;};
+       else
+       {
+           if(i==0) j++;
+           else i=A[--i];
+       }
+
+       if(i==len_pattern) { cnt++; j=j-i};
+    }
+}
+
+void suffix_array()
+{
+    int i=0; int j=1;
+    A[0]=0;
+    while(j<len_pattern)
+    {
+       if(pattern[i]==pattern[j]) A[j++]= ++i;
+       else
+       {
+           if(i==0) j++;
+           else i=A[--i];
+       }
+    }
+
+}
+
+/*
+void printarray()
+{
+    for(int i=0;i<len_text;i++)
+           printf("%d , ",A[i]);
+}
+*/
+int main()
+{   int tc;
+    while(scanf("%d",&tc)==1)
+    {
+        for(int t=1;t<=tc;t++)
+        {
+             scanf("%s",&text); len_text = strlen(text);
+             scanf("%s",&pattern); len_pattern = strlen(text);
+             suffix_array();
+
+        }
+    }
+    return 0;
+}
+
+/*
+acacabacacabacacac
+
+*/
+
